8e16957dc44a1968879e7f3405c4f9a280023c4c
Dima <60237714+overdozzaa@users.noreply.github.com>
2020-02-21 15:49:19 +0200

    Delete main.js

--- JS-HTML-CSS_8HW-JS_1_Parylo/HW_1 (UserBox)/main.js
+++
@@ -1,9 +0,0 @@
-let name = prompt('Write your name');
-let mainH = prompt('Main div height');
-let mainW = prompt('Main div width');
-let mainBGC = prompt('Main div background color');
-let smallH = prompt('Small div height');
-let smallW = prompt('Small div width');
-let smallBG = prompt('Small div background');
-
-document.write(`<div style='width:${mainW}px; height:${mainH}px; background-color:${mainBGC}; display:flex; flex-flow: column wrap'><div style="height:${smallH}px; width:${smallW}px; background-image:url(${smallBG}); background-size:cover; margin-top:24px;"></div><span style="font-size:26px; font-style:bold; margin-top:28px;">Welcome, ${name}</span></div>`)
